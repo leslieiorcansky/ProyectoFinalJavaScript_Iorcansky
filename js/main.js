d75b9a7f3b556a01453746f3fb5bcf445b84f7ad
Leslie Iorcansky <leslieior@hotmail.com>
2022-09-22 08:41:54 -0300

    mejorando el buscador de productos

--- js/main.js
+++ js/main.js
@@ -66,15 +66,16 @@ verCarrito.addEventListener('click', () => {
   const total = carrito.reduce((acc,el) => acc + el.precio,0);
   const totalCompra =document.createElement('div');
   totalCompra.className = 'totalContenido';
-  totalCompra.innerHTML = `Total a pagar: ${total}`;
+  totalCompra.innerHTML = `Total a pagar: $${total}`;
   planillaContenido.append(totalCompra);
 });
 
 
 //BUSCADOR
 const buscador =document.getElementById('buscador');
-const boton =document.getElementById('boton');
 const resultado = document.getElementById('resultado');
+const bars_search = document.getElementById("ctn-bars-search");
+const cover_ctn_search =  document.getElementById("cover-ctn-search");
 
 const filtrar = () => {
   resultado.innerHTML = '';
@@ -85,9 +86,7 @@ const filtrar = () => {
     let nombre = producto.nombre.toLowerCase();
     if(nombre.indexOf(texto) !== -1){
       resultado.innerHTML += `
-      <li>${producto.nombre}</li>
-      <li>$${producto.precio}</li>
-      <li><img src='${producto.img}'></li>
+      <li>${producto.nombre}      $${producto.precio}      <img src='${producto.img}'> </li>
       `
     }
   }
@@ -97,7 +96,17 @@ const filtrar = () => {
     `
   }
 }
-boton.addEventListener('click', filtrar)
+
 buscador.addEventListener('keyup', filtrar)
 
 filtrar();
+ocultar_buscador();
+
+function ocultar_buscador(){
+  bars_search.style.top = "-10px";
+  resultado.innerHTML = '';
+  buscador.value = "";
+}
+
+
+buscador.addEventListener('click',ocultar_buscador);
